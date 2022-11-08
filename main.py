ef72e32d3af25d82703a46b6fc52d6c0761bd94d
victorstefan28 <116149427+victorstefan28@users.noreply.github.com>
2022-11-08 23:00:31 +0200

    Update
    Pt fiecare cuvant din dictionar si fiecare pattern se genereaza match-urile posibile(nu se include pattern_dict.p -> 650mb)

---
+++ main.py
@@ -0,0 +1,80 @@
+import math
+import itertools
+import pickle
+import os
+from scipy.stats import entropy
+from collections import defaultdict
+
+
+def pattern(guess, word):  #1 = galben, 2 = verde, 0 = gri -> match intre doua cuvinte
+    patt = [2,2,2,2,2]
+    for i in range(len(word)):
+        x = guess[i]
+        if x != word[i]:
+            if x in word:
+                patt[i] = 1
+            else:
+                patt[i] = 0
+    return tuple(patt)
+
+def pattern_dict(wordl_dict):
+    pt_dict = defaultdict(lambda: defaultdict(set))
+    for x in wordl_dict:
+        for y in wordl_dict:
+            ptn = pattern(x,y)
+            pt_dict[x][ptn].add(y)
+    return dict(pt_dict)
+
+def get_possible_words(guess, ret_patt, words):
+    pos_word = words
+    for i in range(1,6):
+        if ret_patt[i] == 0:
+            for x in pos_word:
+                if guess[i] in x:
+                    pos_word.remove(x)
+        if ret_patt[i] == 1:
+            for x in pos_word:
+                if guess[i] not in x:
+                    pos_word.remove(x)
+        if ret_patt[i] == 2:
+            for x in pos_word:
+                if x[i] != guess[i]:
+                    pos_word.remove(x)
+    return
+
+
+def intersect(words):
+    return
+
+
+def entropie(words, possible_words, all_patterns, dict):
+    entropies = {}
+    for word in words:
+        cnt = []
+        for pat in all_patterns:
+            return
+
+
+
+def main():
+    s = input("s=")
+    all_pat = list(itertools.product([0,1,2], repeat=5))
+    [print(i) for i in all_pat]
+    f = open("cuvinte_wordle.txt", "r")
+    L = []
+    for x in f:
+        x = x.strip()
+        L.append(x)
+        print(x)
+    f.close()
+    all_dict = {}
+    if 'pattern_dict.p' in os.listdir('.'):
+        all_dict = pickle.load(open('pattern_dict.p', 'rb'))
+    else:
+        all_dict = pattern_dict(L)
+        pickle.dump(all_dict, open('pattern_dict.p', 'wb+'))
+    print(all_dict)
+
+if __name__ == '__main__':
+    main()
+
